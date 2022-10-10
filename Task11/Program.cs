3ba9222eecaf6468aba15f33b7177ec68f1cd4a8
Ruslan <ruslanaldarev0@gmail.com>
2022-10-10 13:35:16 +0500

    добавил задачи

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 11
+Console.WriteLine("Найдите первое и третье число");
+int number = new Random().Next(100 , 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"Итог: {result}");
