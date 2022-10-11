96c62db2cf9f8f1051cff5b281cc18e037d655ea
Ruslan <ruslanaldarev0@gmail.com>
2022-10-11 15:54:44 +0500

    добавил задачи

---
+++ Task16/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 16
+
+void square(int num1, int num2)
+{
+    int sqr = 0;
+    if (num1 < num2)
+    {
+        sqr = num1 * num1;
+        if (sqr == num2)
+        {
+            Console.WriteLine($"Число {num2} является квадратом числа {num1}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num2} не является квадратом числа {num1}");
+        }
+    }
+    else
+    {
+        sqr = num2 * num2;
+        if (sqr == num1)
+        {
+            Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num1} не является квадратом числа {num2}");
+        }
+    }
+}
+
+Console.WriteLine("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+square(firstnumber, secondnumber);
