c0281c1bcee1fd9cfd90e327620b4cdfaa1d6436
Ruslan <ruslanaldarev0@gmail.com>
2022-10-12 13:14:53 +0500

    добавил задачи

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.WriteLine("Найдите вторую цифру из трёхзначного числа: ");
+int number = new Random().Next(100 , 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+int secondDigit = number / 100;
+int thirdDigit = number % 10;
+int result = secondDigit * 10 + thirdDigit;
+Console.WriteLine($"Итог: {result}");
+
+
