d7a4708a3a80a40bf891b309b924883b4901b342
Ruslan <ruslanaldarev0@gmail.com>
2022-10-14 13:00:12 +0500

    добавил задачи

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18
+
+Console.WriteLine("Введите номер четверти");
+string QuaterNum = Console.ReadLine();
+
+string Range(string num)
+{
+    string a = "Диапозон координат находится в пределах";
+
+    if(num == "1") return $"{a} -> Первой четверти";
+    if(num == "2") return $"{a} -> Второй четверти";
+    if(num == "3") return $"{a} -> Третий четверти";
+    if(num == "4") return $"{a} -> Четвертой четверти";
+    return "Введены неверные данные";
+}  
+
+string range = Range(QuaterNum);
+Console.WriteLine(range);
